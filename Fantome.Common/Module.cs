0b0b2b346098f5f91bca2239f99060a419a5a713
Chewy <chewybomber@gmail.com>
2017-07-04 22:25:45 +0200

    Added Settings + Module classes

---
+++ Fantome.Common/Module.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Fantome.Common
+{
+    public abstract class Module
+    {
+        public readonly Settings.ModuleSettings Settings;
+
+        public Module(Settings.ModuleSettings settings)
+        {
+            this.Settings = settings;
+        }
+        
+        public abstract UserControl GetView();
+    }
+
+    //Example
+    public class ModificationInstaller : Module
+    {
+
+        public ModificationInstaller(Settings.ModuleSettings settings) : base(settings)
+        {
+            //Do some stuff here
+        }
+
+        public override UserControl GetView()
+        {
+            // Create user control here..
+            return null;
+        }
+    }
+}
---
+++ Fantome.Common/Settings.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Fantome.Common
+{
+    public class Settings
+    {
+        public readonly List<ModuleSettings> Entries;
+
+        public Settings()
+        {
+            this.Entries = new List<ModuleSettings>();
+        }
+
+        public Settings(String location)
+        {
+            this.Entries = JsonConvert.DeserializeObject<List<ModuleSettings>>(File.ReadAllText(location));
+        }
+
+        public void Save(String location)
+        {
+            File.WriteAllText(location, JsonConvert.SerializeObject(this.Entries, Formatting.Indented));
+        }
+
+        public void AddModuleSettings(string moduleName)
+        {
+            if (this.GetModuleSettings(moduleName) != null)
+            {
+                throw new ModuleSettingsAlreadyExistingException(moduleName);
+            }
+            this.Entries.Add(new ModuleSettings(this, moduleName));
+        }
+
+        public ModuleSettings GetModuleSettings(string moduleName)
+        {
+            return this.Entries.Find(x => String.Equals(moduleName, x.ModuleName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class ModuleSettings
+        {
+            public readonly Settings ParentSettings;
+            public readonly string ModuleName;
+            public readonly Dictionary<String, object> Entries = new Dictionary<String, object>();
+
+            public ModuleSettings(Settings parentSettings, string moduleName)
+            {
+                this.ParentSettings = parentSettings;
+                this.ModuleName = moduleName;
+            }
+        }
+
+        public class ModuleSettingsAlreadyExistingException : Exception
+        {
+            public ModuleSettingsAlreadyExistingException(string moduleName) : base(moduleName + " already has settings") { }
+        }
+    }
+}
